169547ea937faebfd11b37f8087515cbd16b5fed
Alaroudi <alaroudi.abdel@gmail.com>
2021-10-13 15:50:33 -0500

    Redo Sing in page

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
-import {
-  Button,
-  Container,
-  TextField,
-  Typography,
-  Box,
-  InputAdornment,
-  IconButton
-} from "@mui/material";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import LoginIcon from "@mui/icons-material/Login";
-import { Link } from "react-router-dom";
-
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LockIcon from "@mui/icons-material/Lock";
+import "./SignIn.css";
 const SignIn = () => {
   const [values, setValues] = useState({
     email: "",
@@ -30,73 +29,85 @@ const SignIn = () => {
   };
 
   return (
-    <Container
-      maxWidth="sm"
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        height: "100vh"
-      }}
-    >
-      <Typography variant="h2" textAlign="center" mb={"100px"}>
-        Sign in
-      </Typography>
-      <Box
-        onSubmit={handleSubmit}
-        component="form"
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-
-          "& > :not(style)": { mb: 3, width: "80%" }
-        }}
-      >
-        <TextField
-          variant="outlined"
-          label="Email"
-          id="email"
-          type="email"
-          required
-          value={values.email}
-          onChange={handleChange}
-        />
-        <TextField
-          variant="outlined"
-          label="Password"
-          id="password"
-          type={showPassword ? "text" : "password"}
-          required
-          value={values.password}
-          onChange={handleChange}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton onClick={() => setShowPassword(!showPassword)}>
-                  {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                </IconButton>
-              </InputAdornment>
-            )
-          }}
-        />
-
-        <Link to="/signup" style={{ textDecoration: "none", fontSize: "1rem" }}>
-          Create Account
-        </Link>
-
-        <Button
-          variant="contained"
-          type="submit"
-          size="large"
-          endIcon={<LoginIcon />}
-          sx={{ borderRadius: "20px" }}
-        >
-          Sign in
-        </Button>
-      </Box>
-    </Container>
+    <div className="signin-container">
+      <div className="form-container">
+        <div style={{ width: "80%" }}>
+          <Typography variant="h2" textAlign="center" mb={"100px"}>
+            Sign in
+          </Typography>
+          <Box
+            onSubmit={handleSubmit}
+            component="form"
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              "& > :not(style)": { mb: 3 }
+            }}
+          >
+            <TextField
+              variant="outlined"
+              label="Email"
+              id="email"
+              type="email"
+              fullWidth
+              required
+              value={values.email}
+              onChange={handleChange}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <AccountCircleIcon />
+                  </InputAdornment>
+                )
+              }}
+            />
+            <TextField
+              variant="outlined"
+              label="Password"
+              id="password"
+              fullWidth
+              required
+              type={showPassword ? "text" : "password"}
+              value={values.password}
+              onChange={handleChange}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <LockIcon />
+                  </InputAdornment>
+                ),
+                endAdornment: values.password && (
+                  <InputAdornment position="end">
+                    <IconButton onClick={() => setShowPassword(!showPassword)}>
+                      {showPassword ? (
+                        <VisibilityIcon />
+                      ) : (
+                        <VisibilityOffIcon />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}
+            />
+            <Link to="/signup" className="link">
+              Create Account
+            </Link>
+            <Button
+              variant="contained"
+              type="submit"
+              size="large"
+              endIcon={<LoginIcon />}
+              sx={{ borderRadius: "20px", mt: "1rem" }}
+            >
+              Sign in
+            </Button>
+          </Box>
+        </div>
+        <div>
+          <img src="./images/login.svg" alt="" width="100%" />
+        </div>
+      </div>
+    </div>
   );
 };
 
