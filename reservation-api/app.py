758e3202730e7a8f573c3e996d31c77cb2a8e11e
Christina Zhang <christinazhang2013@gmail.com>
2021-10-21 09:45:56 -0500

    RESTful API in Flask for bookings

--- reservation-api/app.py
+++ reservation-api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, request, redirect, url_for
+from flask import Flask, render_template, flash, request, redirect, url_for, jsonify
 from datetime import datetime
 # SQLAlchemy imports
 from flask_sqlalchemy import SQLAlchemy
@@ -18,12 +18,12 @@ app = Flask(__name__)
 
 # add database
 # replace password with your servers password
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Trash_Panda1@localhost/hotel_reservation'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234567890@localhost/hotel_reservation'
 
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="Trash_Panda1"
+    passwd="1234567890"
 )
 
 cursor = mydb.cursor()
@@ -36,8 +36,6 @@ cursor = mydb.cursor()
 # connectes to the actual database for flask to make the pages
 db = SQLAlchemy(app)
 
-# Automap will create equivalent classes (as opposed to reflection, which only allows querying)
-# so that we can create/delete rows
 Base = automap_base()
 Base.prepare(db.engine, reflect=True)
 Reservations = Base.classes.reservations
@@ -46,193 +44,81 @@ Reserved_Room_Type = Base.classes.reserved_room_type
 Hotel = Base.classes.hotel
 
 
-# names of hotels match id's
-hotel_names = {}
-reverse_hotel_names = {}
-hotel_list = db.session.query(Hotel).order_by(Hotel.hotel_id).all()
+@app.route('/bookings', methods=['GET'])
+def get_all_reservations():
+    reservations = db.session.query(Reservations).all()
 
-for i in hotel_list:
-    hotel_names[i.hotel_id] = str(i.hotel_name)
-    reverse_hotel_names[str(i.hotel_name)] = i.hotel_id
+    output = []
+    for res in reservations:
+        res_data = {}
+        res_data['reservation_id'] = res.reservation_id
+        res_data['user_id'] = res.user_id
+        res_data['hotel_id'] = res.hotel_id
+        res_data['check_in'] = res.check_in
+        res_data['check_out'] = res.check_out
+        res_data['total_price'] = res.total_price
+        res_data['status'] = res.status
+        output.append(res_data)
 
-
-# engine = create_engine(
-#    'mysql+pymysql://root:1234567890@localhost/hotel_reservation').connect()
-
-# initial reservation (preliminary test of session adding and committing)
-# datetime is formatted as year, month, day, hour, minute
-'''
-for i in range(1, 10):
-    if i % 5 == 1:
-        initial_res = Reservations(reservation_id=1 + i, user_id=1, hotel_id=i+1,
-                                   check_in=datetime(2021, 11, 15), check_out=datetime(2021, 11, 16), total_price=5000 + 50 * i, status=0)
-    elif i % 5 == 2:
-        initial_res = Reservations(reservation_id=1 + i, user_id=2, hotel_id=i,
-                                   check_in=datetime(2021, 11, 15), check_out=datetime(2021, 11, 16), total_price=5000 + 50 * i, status=0)
-    elif i % 5 == 3:
-        initial_res = Reservations(reservation_id=1 + i, user_id=2, hotel_id=2,
-                                   check_in=datetime(2021, 11, 15), check_out=datetime(2021, 11, 16), total_price=5000 + 50 - i, status=0)
-    elif i % 5 == 4:
-        initial_res = Reservations(reservation_id=1 + i, user_id=3, hotel_id=4,
-                                   check_in=datetime(2021, 11, 15), check_out=datetime(2021, 11, 16), total_price=5000 + 50 + i, status=0)
-    else:
-        initial_res = Reservations(reservation_id=1 + i, user_id=4, hotel_id=10,
-                                   check_in=datetime(2021, 11, 15), check_out=datetime(2021, 11, 16), total_price=5000 + 50 / i, status=0)
-
-    db.session.add(initial_res)
-
-db.session.commit()
-'''
-# results = db.session.query(Reservations).all()
-# for r in results:
-#    print(r.check_out)
+    return jsonify({'bookings': output})
 
 
-# function to load reservations belonging to user_id
-def loadUsersRes(user_id):
-    return db.session.query(Reservations).filter_by(user_id=user_id).all()
+@app.route('/bookings/<id>', methods=['GET'])
+def get_one_reservation(id):
+    res = db.session.query(Reservations).filter_by(reservation_id=id).first()
+
+    if not res:
+        return jsonify({'message': 'No booking found!'})
+
+    res_data = {}
+    res_data['reservation_id'] = res.reservation_id
+    res_data['user_id'] = res.user_id
+    res_data['hotel_id'] = res.hotel_id
+    res_data['check_in'] = res.check_in
+    res_data['check_out'] = res.check_out
+    res_data['total_price'] = res.total_price
+    res_data['status'] = res.status
+
+    return jsonify({'bookings': res_data})
 
 
-# function to load the reservation with reservation_id
-def loadResByResID(reservation_id):
-    return db.session.query(Reservations).filter_by(reservation_id=reservation_id).first()
+@app.route('/bookings', methods=['POST'])
+def create_reservation():
+    data = request.get_json()
 
-
-# Update Functions
-###################################
-# updates a reservation for a customer
-@app.route('/updateRes', methods=['POST'])
-def updateRes():
-    formID = request.form.get('formID')
-    newCheckIn = request.form.get("newCheckIn")
-    newCheckOut = request.form.get("newCheckOut")
-
-    # fetch the reservation with reservation_id (formID)
-    res = None
-    res = loadResByResID(formID)
-
-    if newCheckIn == "None" or not newCheckIn:
-        res.check_in = None
-    else:
-        res.check_in = newCheckIn
-
-    if newCheckOut == "None" or not newCheckOut:
-        res.check_out = None
-    else:
-        res.check_out = newCheckOut
-
-    db.session.add(res)
+    new_reservation = Reservations(reservation_id=data['reservation_id'], user_id=data['user_id'], hotel_id=data['hotel_id'],
+                                   check_in=data['check_in'], check_out=data['check_out'], total_price=data['total_price'], status=data['status'])
+    db.session.add(new_reservation)
     db.session.commit()
 
-    return redirect("/dashboard")
+    return jsonify({'message': 'New booking created.'})
 
 
-# Delete Functions
-###################################
-@app.route('/deleteRes', methods=['POST'])
-def deleteRes():
-    formID = request.form.get('formID')
+@app.route('/bookings/<id>', methods=['PUT'])
+def promote_status_reservation(id):
+    res = db.session.query(Reservations).filter_by(reservation_id=id).first()
 
-    # fetch the reservation with reservation_id (formID)
-    res = None
-    res = loadResByResID(formID)
+    if not res:
+        return jsonify({'message': 'No booking found!'})
+
+    res.status = 1
+    db.session.commit()
+
+    return jsonify({'message': 'Status on booking has changed!'})
+
+
+@app.route('/bookings/<id>', methods=['DELETE'])
+def delete_reservation(id):
+    res = db.session.query(Reservations).filter_by(reservation_id=id).first()
+
+    if not res:
+        return jsonify({'message': 'No booking found!'})
 
     db.session.delete(res)
     db.session.commit()
 
-    return redirect("/dashboard")
+    return jsonify({'message': 'Booking has been deleted.'})
 
-# CREATE FUNCTIONS
-######################################
-@app.route('/createRes', methods=['POST'])
-def add_res():
-    user_id = request.form.get("userID")
-    hotel = request.form.get("hotel")
-    checkIn = request.form.get("checkIn")
-    checkOut = request.form.get("checkOut")
-    num_room = int(request.form.get("num_room"))
-    standard = int(request.form.get("standard"))
-    queen = int(request.form.get("queen"))
-    king = int(request.form.get("king"))
-    
-    if checkIn:
-        if checkOut:
-            if num_room > 0:
-                if standard + queen + king == num_room:
-                    res = Reservations(user_id=user_id, hotel_id=reverse_hotel_names[hotel],
-                                   check_in=checkIn, check_out=checkOut, total_price=5000 + 50, status="CONFIRMED")
-        #         else:
-        #             flash("ERROR PLEASE SELECT THE TYPE OF ROOMS YOU WOULD LIKE")
-        #             return render_template('add_res.html', userID=user_id, hotel_options=hotel_names.values())
-
-        #     else:
-        #         flash("ERROR PLEASE ENTER THE NUMBER OF ROOMS WANTED MIN:1")
-        #         return render_template('add_res.html', userID=user_id, hotel_options=hotel_names.values())
-        # else:
-        #     flash("ERROR PLEASE SELECT A CHECK OUT DATE")
-        #     return render_template('add_res.html', userID=user_id, hotel_options=hotel_names.values())
-    else:
-        #flash("ERROR PLEASE SELECT A CHECK IN DATE")
-        return render_template('add_res.html', userID=user_id, hotel_options=hotel_names.values())
-    
-    db.session.add(res)
-    db.session.commit()
-    return redirect("/dashboard")
-
-
-# URL Routes
-###################################
-
-
-# home page for reservations API
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-
-# this page shows the customers reservations
-@app.route('/dashboard', methods=['GET'])
-def dashboard(id=2, admin=""):
-    # fetch ALL reservations that belong to userID
-    usersRes = []
-    usersRes = loadUsersRes(id)
-
-    for i in usersRes:
-        i.hotel_id = hotel_names[i.hotel_id]
-
-    return render_template("dashboard.html", usersRes=usersRes)
-
-
-# form for customer to update reservations
-@app.route('/updateResForm', methods=['GET', 'POST'])
-def updateResForm():
-
-    formID = request.args.get('form')
-    # fetch the reservation with reservation_id (formID)
-    res = None
-    res = loadResByResID(formID)
-    return render_template("updateResForm.html", res=res, formID=formID)
-
-
-# confirmation for customer to delete reservation
-@app.route('/deleteResConfirmation', methods=['GET', 'POST'])
-def deleteResConfirmation():
-
-    formID = request.args.get('form')
-    # fetch the reservation with reservation_id (formID)
-    res = None
-    res = loadResByResID(formID)
-    return render_template("deleteResConfirmation.html", res=res, formID=formID)
-
-# form to create reservation
-@app.route('/add_res', methods=['GET', 'POST'])
-def add_res_form():
-
-    userID = request.args.get('ID')
-    hotel_options = list(hotel_names.values())
-
-
-    return render_template("add_res.html", userID=userID, hotel_options=hotel_options)
 
 if __name__ == "__main__":
 
