6fb7c2cc9b3b7c226736c192e9b719cdc615b935
Dillon Dakil <DillonDakil@gmail.com>
2021-10-27 17:07:18 -0700

    get reservation based on user_id and get all user info and reservations associated

--- reservation-api/app.py
+++ reservation-api/app.py
@@ -1,3 +1,4 @@
+from sqlalchemy.sql.functions import user
 from database import *
 from flask import Flask, request, abort, jsonify
 from flask_restful import Api, Resource
@@ -36,63 +37,6 @@ def generate_model(host, user, password, database, outfile=None):
     Session = sessionmaker(bind=engine)
     session = Session()
 
-
-# function to set up amenities list
-# returns a list of amenities that the hotel has
-
-
-def generate_amenities(hotel):
-    amenities = []
-    # check if hotel has pool
-    if hotel.Pool:
-        amenities.append("Pool")
-    # check if hotel has gym
-    if hotel.Gym:
-        amenities.append("Gym")
-    # check if hotel has spa
-    if hotel.Spa:
-        amenities.append("Spa")
-    # check if hotel has business office
-    if hotel.Bussiness_Office:
-        amenities.append("Business Office")
-    # check if hotel as wifi
-    if hotel.Wifi:
-        amenities.append("Wifi")
-    return amenities
-
-
-# function to set up room_types list
-# returns a list of dictionaries with the information of each room_type in them
-
-
-def generate_rooms(hotel):
-    # set up room_type list
-    rooms = []
-    # set up standard inner dictionary
-    standard_information = {}
-    standard_information["price"] = float(hotel.standard_price)
-    standard_information["count"] = hotel.standard_count
-    # set up queen inner dictionary
-    queen_information = {}
-    queen_information["price"] = float(hotel.queen_price)
-    queen_information["count"] = hotel.queen_count
-    # set up king inner dictionary
-    king_information = {}
-    king_information["price"] = float(hotel.king_price)
-    king_information["count"] = hotel.king_count
-    # set up standard outer dictionary and add to rooms
-    standard_dict = {}
-    standard_dict["Standard"] = standard_information
-    rooms.append(standard_dict)
-    # set up queen outer dictionary and add to rooms
-    queen_dict = {}
-    queen_dict["Queen"] = queen_information
-    rooms.append(queen_dict)
-    # set up king outer dictionary and add to rooms
-    king_dict = {}
-    king_dict["King"] = king_information
-    rooms.append(king_dict)
-    return rooms
 
 
 # function to create a new hotel in the database
@@ -223,78 +167,127 @@ def generate_new_hotel():
     return new_hotel
 
 
-# function to set a valid json for a hotel object
-# returns a list of dictionaries depending on the hotel_results query
-# outputs each hotel in this format:
+# function to set a valid json for user object
+# returns a list of dictionaries depending on the user query
+# outputs each user and reservation in this format:
 '''
 {
-    "hotel_id": 2,
-    "hotel_name": "The Magnolia All Suites",
-    "street_address": "14187 Commercial Trail",
-    "city": "Hampton",
-    "state": "VA",
-    "zipcode": 23452,
-    "phone_number": "213-342-5433",
-    "weekend_diff_percentage": 0.25,
-    "number_of_rooms": 20,
-    "amenities": [
-        "Pool",
-        "Gym",
-        "Spa",
-        "Business Office"
-    ],
-    "room_types": [
-        {
-            "Standard": {
-                "price": 100.0,
-                "count": 10
-            }
+    "user_id": 1,
+    "first_name": "John",
+    "last_name": "Doe",
+    "email": "John.Doe@yahoo.com",
+    "password": "1234",
+    "isAdmin": 0,
+    "phone_number": "210-222-2222",
+    "date_of_birth": 2000-12-12,
+    "reservations": [
+        {   
+            "reservation_id": 1,
+            "user_id": 1,
+            "hotel_id": 1,
+            "check_in": "2021-12-12",
+            "check_out": "2021-12-15",
+            "total_price": 100.56,
+            "reserved_standard_count": "1",
+            "reserved_queen_count": 0,
+            "reserved_king_count": 1
         },
         {
-            "Queen": {
-                "price": 150.0,
-                "count": 5
-            }
-        },
-        {
-            "King": {
-                "price": 250.0,
-                "count": 5
-            }
+            "reservation_id": 12,
+            "user_id": 1,
+            "hotel_id": 4,
+            "check_in": "2022-1-12",
+            "check_out": "2022-1-25",
+            "total_price": 1000.56,
+            "reserved_standard_count": "1",
+            "reserved_queen_count": 1,
+            "reserved_king_count": 12
         }
     ]
 }
 '''
 
 
-def generate_hotel_entry(hotel_results):
+def generate_user_entry(user_results):
     # set up list to return
     result_list = []
-    for hotel in hotel_results:
+    for cur_user in user_results:
         # set up dictionary to be added to result list
         new_entry = {}
+        
         # enter each respective variable into the dictionary
-        new_entry["hotel_id"] = hotel.hotel_id
-        new_entry["hotel_name"] = hotel.hotel_name
-        new_entry["street_address"] = hotel.street_address
-        new_entry["city"] = hotel.city
-        new_entry["state"] = hotel.state
-        new_entry["zipcode"] = hotel.zipcode
-        new_entry["phone_number"] = hotel.phone_number
-        new_entry["weekend_diff_percentage"] = float(
-            hotel.weekend_diff_percentage)
-        # calculate total number of rooms
-        num_standard = hotel.standard_count
-        num_queen = hotel.queen_count
-        num_king = hotel.king_count
-        total_rooms = num_standard + num_queen + num_king
-        new_entry["number_of_rooms"] = total_rooms
-        # set up amenities list
-        amenities_list = generate_amenities(hotel)
-        new_entry["amenities"] = amenities_list
-        # set up room_types list
-        room_types_list = generate_rooms(hotel)
-        new_entry["room_types"] = room_types_list
+        new_entry["user_id"] = cur_user.user_id
+        new_entry["first_name"] = cur_user.first_name
+        new_entry["last_name"] = cur_user.last_name
+        new_entry["email"] = cur_user.email
+        new_entry["password"] = cur_user.password
+        new_entry["isAdmin"] = cur_user.isAdmin
+        new_entry["phone_number"] = cur_user.phone_number
+        new_entry["date_of_birth"] = str(cur_user.date_of_birth)
+
+        # set up reservations
+        reservation_info = generate_reservation_entry(cur_user.user_id)
+        new_entry["reservations"] = reservation_info
+        
+        # append the new_entry into results if it is not already added
+        if new_entry not in result_list:
+            result_list.append(new_entry)
+    # return results
+    return result_list
+
+# function to set a valid json for a user_id
+# returns a list of dictionaries depending on the user_id query
+# outputs each reservastion in this format:
+'''
+{
+    [
+        {   
+            "reservation_id": 1,
+            "user_id": 1,
+            "hotel_id": 1,
+            "check_in": "2021-12-12",
+            "check_out": "2021-12-15",
+            "total_price": 100.56,
+            "reserved_standard_count": "1",
+            "reserved_queen_count": 0,
+            "reserved_king_count": 1
+        },
+        {
+            "reservation_id": 12,
+            "user_id": 1,
+            "hotel_id": 4,
+            "check_in": "2022-1-12",
+            "check_out": "2022-1-25",
+            "total_price": 1000.56,
+            "reserved_standard_count": "1",
+            "reserved_queen_count": 1,
+            "reserved_king_count": 12
+        }
+    ]
+}
+'''
+
+
+def generate_reservation_entry(user_id):
+    user_reservation = session.query(Reservation).filter(Reservation.user_id == user_id).order_by(Reservation.reservation_id).all()
+
+    # set up list to return
+    result_list = []
+    for res in user_reservation:
+        # set up dictionary to be added to result list
+        new_entry = {}
+        
+        # enter each respective variable into the dictionary
+        new_entry["reservation_id"] = res.reservation_id
+        new_entry["user_id"] = res.user_id
+        new_entry["hotel_id"] = res.hotel_id
+        new_entry["check_in"] = str(res.check_in)
+        new_entry["check_out"] = str(res.check_out)
+        new_entry["total_price"] = float(res.total_price)
+        new_entry["reserved_standard_count"] = res.reserved_standard_count
+        new_entry["reserved_queen_count"] = res.reserved_queen_count
+        new_entry["reserved_king_count"] = res.reserved_king_count
+
         # append the new_entry into results if it is not already added
         if new_entry not in result_list:
             result_list.append(new_entry)
@@ -302,84 +295,49 @@ def generate_hotel_entry(hotel_results):
     return result_list
 
 
-## ---------- Hotels ---------- ##
-# class for interacting with all hotels in the database
 
 
-class AllHotels(Resource):
+## ---------- Admin ---------- ##
+# class for interacting with all Reservations in the database
+
+
+class AllReservations(Resource):
 
     # function to get all hotels from the database
     def get(self):
         # query to get all hotels
-        hotels = session.query(Hotel).order_by(Hotel.hotel_id).all()
+        all_users = session.query(User).order_by(User.user_id).all()
         # generate a list from hotels
-        result = generate_hotel_entry(hotels)
+        result = generate_user_entry(all_users)
 
         # if there are no hotels, show error
         if not result:
-            abort(404, description="There are no hotels in the database.")
+            abort(404, description="There are no users in the database.")
         # return the results
         return result
 
-    # function to add a new hotel to the database
-    def post(self):
-        # generate the new Hotel object
-        new_hotel = generate_new_hotel()
-
-        # if new_hotel is an int, the hotel already exists
-        if isinstance(new_hotel, int):
-            abort(409, f"Hotel ID {new_hotel} already exists in the database.")
-
-        # add and commit the new hotel to database
-        session.add(new_hotel)
-        session.commit()
-        # return success message
-        return {
-            "message":
-            f"Hotel ID {new_hotel.hotel_id} was successfully added to the database."
-        }
 
 
-# TODO: PUT for SingleHotel
+## ---------- User ---------- ##
+# class for interacting with user Reservations in the database
 
-# class for interacting with one hotel in the database
+class UserReservation(Resource):
+    
+    # function to get a user reservations based on user_id from the database
+    def get(self, user_id):
 
-
-class SingleHotel(Resource):
-
-    # function to delete a single hotel from the database by ID number
-    def delete(self, hotel_id):
-        # select the correct instance
-        result = session.query(Hotel).get(hotel_id)
-
-        # if there is no instance that matches the ID, show error message
-        if not result:
-            abort(404,
-                  description=
-                  f"Hotel ID {hotel_id} does not exist in the database.")
-
-        # delete the hotel and commit
-        session.delete(result)
-        session.commit()
-
-        # return message on success
-        return {"message": f"Hotel ID {hotel_id} was successfully deleted."}
-
-    # function to get a single hotel from the database by ID number
-    def get(self, hotel_id):
-        # query to get the hotel and filter by hotel number
-        hotels = session.query(Hotel).filter(Hotel.hotel_id == hotel_id).all()
         # generate a list from hotels
-        result = generate_hotel_entry(hotels)
+        result = generate_reservation_entry(user_id)
 
         # if there are no hotels, show error
         if not result:
             abort(404,
                   description=
-                  f"Hotel ID {hotel_id} does not exist in the database.")
+                  f"USER ID {user_id} does not exist in the database.")
 
         # return the result
-        return result[0]
+        return result
+    
 
 
 class SingleBooking(Resource):
@@ -402,10 +360,13 @@ class SingleBooking(Resource):
             f"Reservation ID {reservation_id} was successfully deleted."
         }
 
+    
+
+    
 
 # add to each class to API
-api.add_resource(SingleHotel, "/api/hotels/<int:hotel_id>")
-api.add_resource(AllHotels, "/api/hotels")
+api.add_resource(UserReservation, "/api/user/<int:user_id>")
+api.add_resource(AllReservations, "/api/user")
 api.add_resource(SingleBooking, "/api/bookings/<int:reservation_id>")
 
 if __name__ == "__main__":
