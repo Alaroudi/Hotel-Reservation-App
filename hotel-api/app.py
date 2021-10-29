f92e5d4d79be6ad6acc16f35615ad641447482e2
emiliomanuel <45859128+emiliomanuel@users.noreply.github.com>
2021-10-29 17:59:17 -0500

    Updated to reflect GET and PUT outputs.
    Changed them to match the database.

--- hotel-api/app.py
+++ hotel-api/app.py
@@ -20,7 +20,12 @@ hotel_post_args.add_argument("zipcode", type = int, help = "Enter the zipcode of
 hotel_post_args.add_argument("phone_number", type = str, help = "Enter phone number name of the hotel. (string)", required = True)
 hotel_post_args.add_argument("weekend_diff_percentage", type = float, help = "Enter the price differential for the weekend of the hotel. (decimal number)", required = True)
 hotel_post_args.add_argument("amenities", type = list, help = "Enter the amenities of the hotel. (list of strings)", required = True)
-hotel_post_args.add_argument("room_types", type = list, help = "Enter the room type data of the hotel. (list of dictionaries)", required = True)
+hotel_post_args.add_argument("standard_count", type = int, help = "Enter number of standard rooms. (int)", required = True)
+hotel_post_args.add_argument("standard_price", type = float, help = "Enter price of standard rooms. (float)", required = True)
+hotel_post_args.add_argument("queen_count", type = int, help = "Enter number of queen rooms. (int)", required = True)
+hotel_post_args.add_argument("queen_price", type = float, help = "Enter price of queen rooms. (float)", required = True)
+hotel_post_args.add_argument("king_count", type = int, help = "Enter number of king rooms. (int)", required = True)
+hotel_post_args.add_argument("king_price", type = float, help = "Enter price of king rooms. (float)", required = True)
 
 # set up request parser for PUT
 hotel_put_args = reqparse.RequestParser()
@@ -33,6 +38,12 @@ hotel_put_args.add_argument("phone_number", type = str, help = "Enter phone numb
 hotel_put_args.add_argument("weekend_diff_percentage", type = float, help = "Enter the price differential for the weekend of the hotel. (decimal number)")
 hotel_put_args.add_argument("amenities", type = list, help = "Enter the amenities of the hotel. (list of strings)")
 hotel_put_args.add_argument("room_types", type = list, help = "Enter the room type data of the hotel. (list of dictionaries)")
+hotel_put_args.add_argument("standard_count", type = int, help = "Enter number of standard rooms. (int)")
+hotel_put_args.add_argument("standard_price", type = float, help = "Enter price of standard rooms. (float)")
+hotel_put_args.add_argument("queen_count", type = int, help = "Enter number of queen rooms. (int)")
+hotel_put_args.add_argument("queen_price", type = float, help = "Enter price of queen rooms. (float)")
+hotel_put_args.add_argument("king_count", type = int, help = "Enter number of king rooms. (int)")
+hotel_put_args.add_argument("king_price", type = float, help = "Enter price of king rooms. (float)")
 
 # set up list for valid amenities
 valid_amenities = ["Pool", "Gym", "Spa", "Business Office", "Wifi"]
@@ -117,132 +128,23 @@ def generate_hotel_amenities(amenities):
     # return all the values
     return (has_pool, has_gym, has_spa, has_bus, has_wifi)
 
-## function to set up the room_type variables for the Hotel class
-# returns a set of variables for the room_type: its count and its price
-def generate_hotel_room_type(room_types):
-    standard_count = -1
-    standard_price = -1
-    queen_count = -1
-    queen_price = -1
-    king_count = -1
-    king_price = -1
-    # go through the list and store the values of each variable
-    for room in room_types:
-        # values for standard rooms
-        if "Standard" in room:
-            # check to see what values are being updated
-            for room_type, inner in room.items():
-                for key, value in inner.items():
-                    # if count is being updated, store the value
-                    if key == "count":
-                        standard_count = room["Standard"]["count"]
-                    # if price is being updated, store the value
-                    if key == "price":
-                        standard_price = room["Standard"]["price"]
-        # values for queen rooms
-        if "Queen" in room:
-            # check to see what values are being updated
-            for room_type, inner in room.items():
-                for key, value in inner.items():
-                    # if count is being updated, store the value
-                    if key == "count":
-                        queen_count = room["Queen"]["count"]
-                    # if price is being updated, store the value
-                    if key == "price":
-                        queen_price = room["Queen"]["price"]
-        # values for king rooms
-        if "King" in room:
-            # check to see what values are being updated
-            for room_type, inner in room.items():
-                for key, value in inner.items():
-                    # if count is being updated, store the value
-                    if key == "count":
-                        king_count = room["King"]["count"]
-                    # if price is being updated, store the value
-                    if key == "price":
-                        king_price = room["King"]["price"]
-    # return all the values
-    return (standard_count, standard_price, queen_count, queen_price, king_count, king_price)
-
-## function to set up room_types list
-# returns a list of dictionaries with the information of each room_type in them
-def generate_rooms(hotel):
-    # set up room_type list
-    rooms = []
-    # set up standard inner dictionary
-    standard_information = {}
-    standard_information["price"] = float(hotel.standard_price)
-    standard_information["count"] = hotel.standard_count
-    # set up queen inner dictionary
-    queen_information = {}
-    queen_information["price"] = float(hotel.queen_price)
-    queen_information["count"] = hotel.queen_count
-    # set up king inner dictionary
-    king_information = {}
-    king_information["price"] = float(hotel.king_price)
-    king_information["count"] = hotel.king_count
-    # set up standard outer dictionary and add to rooms
-    standard_dict = {}
-    standard_dict["Standard"] = standard_information
-    # if there are standard rooms, add it to the list
-    if hotel.standard_count:
-        rooms.append(standard_dict)
-    # set up queen outer dictionary and add to rooms
-    queen_dict = {}
-    queen_dict["Queen"] = queen_information
-    # if there are queen rooms, add it to the list
-    if hotel.queen_count:
-        rooms.append(queen_dict)
-    # set up king outer dictionary and add to rooms
-    king_dict = {}
-    king_dict["King"] = king_information
-    # if there are king rooms, add it to the list
-    if hotel.king_count:  
-        rooms.append(king_dict)
-    return rooms
-
 ## function to create a new hotel in the database
 # returns a Hotel object with the variables given from POST
 # expects input in this format:
-'''
-{
-    "hotel_name": "The Magnolia All Suites",
-    "street_address": "14187 Commercial Trail",
-    "city": "Hampton",
-    "state": "VA",
-    "zipcode": 23452,
-    "phone_number": "213-342-5433",
-    "weekend_diff_percentage": 0.25,
-    "number_of_rooms": 20,
-    "amenities": [
-        "Pool",
-        "Gym",
-        "Spa",
-        "Business Office"
-    ],
-    "room_types": [
-        {
-            "Standard": {
-                "price": 100.0,
-                "count": 10
-            }
-        },
-        {
-            "Queen": {
-                "price": 150.0,
-                "count": 5
-            }
-        },
-        {
-            "King": {
-                "price": 250.0,
-                "count": 5
-            }
-        }
-    ]
-}
-'''
-def generate_new_hotel(hotel_name, street_address, city, state, zipcode, phone_number, weekend_diff_percentage, amenities, room_types):
+def generate_new_hotel(hotel_name,
+                       street_address,
+                       city,
+                       state,
+                       zipcode,
+                       phone_number,
+                       weekend_diff_percentage,
+                       amenities,
+                       standard_count,
+                       standard_price,
+                       queen_count,
+                       queen_price,
+                       king_count,
+                       king_price):
     # extract data from the list received from amenities
     has_pool = 0
     has_gym = 0
@@ -265,23 +167,33 @@ def generate_new_hotel(hotel_name, street_address, city, state, zipcode, phone_n
         # if the hotel has a wifi, add to hotel
         if amenity == "Wifi":
             has_wifi = 1
-    # extract data from the list received from room_types
-    for room in room_types:
-        if "Standard" in room:
-            standard_count = room["Standard"]["count"]
-            standard_price = room["Standard"]["price"]
-        if "Queen" in room:
-            queen_count = room["Queen"]["count"]
-            queen_price = room["Queen"]["price"]
-        if "King" in room:
-            king_count = room["King"]["count"]
-            king_price = room["King"]["price"]
+    # update room_type information
+    standard_count = standard_count
+    standard_price = standard_price
+    queen_count = queen_count
+    queen_price = queen_price
+    king_count = king_count
+    king_price = king_price
 
     # create a new Hotel object from the information provided
-    new_hotel = Hotel(hotel_name = hotel_name, street_address = street_address, city = city, state = state, zipcode = zipcode, 
-                      phone_number = phone_number, standard_count = standard_count, queen_count = queen_count, king_count = king_count,
-                      standard_price = standard_price, queen_price = queen_price, king_price = king_price, Pool = has_pool, Gym = has_gym,
-                      Spa = has_spa, Bussiness_Office = has_bus, Wifi = has_wifi, weekend_diff_percentage = weekend_diff_percentage)
+    new_hotel = Hotel(hotel_name = hotel_name,
+                      street_address = street_address,
+                      city = city,
+                      state = state, 
+                      zipcode = zipcode, 
+                      phone_number = phone_number, 
+                      standard_count = standard_count, 
+                      queen_count = queen_count, 
+                      king_count = king_count,
+                      standard_price = standard_price, 
+                      queen_price = queen_price, 
+                      king_price = king_price, 
+                      Pool = has_pool, 
+                      Gym = has_gym,
+                      Spa = has_spa,
+                      Bussiness_Office = has_bus, 
+                      Wifi = has_wifi, 
+                      weekend_diff_percentage = weekend_diff_percentage)
 
     # return the new Hotel object
     return new_hotel
@@ -289,45 +201,6 @@ def generate_new_hotel(hotel_name, street_address, city, state, zipcode, phone_n
 ## function to set a valid json for a hotel object
 # returns a list of dictionaries depending on the hotel_results query
 # outputs each hotel in this format:
-'''
-{
-    "hotel_id": 2,
-    "hotel_name": "The Magnolia All Suites",
-    "street_address": "14187 Commercial Trail",
-    "city": "Hampton",
-    "state": "VA",
-    "zipcode": 23452,
-    "phone_number": "213-342-5433",
-    "weekend_diff_percentage": 0.25,
-    "number_of_rooms": 20,
-    "amenities": [
-        "Pool",
-        "Gym",
-        "Spa",
-        "Business Office"
-    ],
-    "room_types": [
-        {
-            "Standard": {
-                "price": 100.0,
-                "count": 10
-            }
-        },
-        {
-            "Queen": {
-                "price": 150.0,
-                "count": 5
-            }
-        },
-        {
-            "King": {
-                "price": 250.0,
-                "count": 5
-            }
-        }
-    ]
-}
-'''
 def generate_hotel_entry(hotel_results):
     # set up list to return
     result_list = []
@@ -353,8 +226,12 @@ def generate_hotel_entry(hotel_results):
         amenities_list = generate_amenities(hotel)
         new_entry["amenities"] = amenities_list
         # set up room_types list
-        room_types_list = generate_rooms(hotel)
-        new_entry["room_types"] = room_types_list
+        new_entry["standard_count"] = hotel.standard_count
+        new_entry["standard_price"] = float(hotel.standard_price)
+        new_entry["queen_count"] = hotel.queen_count
+        new_entry["queen_price"] = float(hotel.queen_price)
+        new_entry["king_count"] = hotel.king_count
+        new_entry["king_price"] = float(hotel.king_price)
         # append the new_entry into results if it is not already added
         if new_entry not in result_list:
             result_list.append(new_entry)
@@ -408,18 +285,28 @@ class AllHotels(Resource):
             weekend_diff_percentage = request.json["weekend_diff_percentage"]
             amenities = request.json["amenities"]
             room_types = request.json["room_types"]
+            standard_count = request.json["standard_count"]
+            standard_price = request.json["standard_price"]
+            queen_count = request.json["queen_count"]
+            queen_price = request.json["queen_price"]
+            king_count = request.json["king_count"]
+            king_price = request.json["king_price"]
 
-            # check to see if the number_of_rooms matches the total from room_types
-            for room in room_types:
-                if "Standard" in room:
-                    standard_count = room["Standard"]["count"]
-                if "Queen" in room:
-                    queen_count = room["Queen"]["count"]
-                if "King" in room:
-                    king_count = room["King"]["count"]
-            
             # generate the new Hotel object
-            new_hotel = generate_new_hotel(hotel_name, street_address, city, state, zipcode, phone_number, weekend_diff_percentage, amenities, room_types)
+            new_hotel = generate_new_hotel(hotel_name,
+                                           street_address,
+                                           city,
+                                           state,
+                                           zipcode,
+                                           phone_number,
+                                           weekend_diff_percentage,
+                                           amenities,
+                                           standard_count,
+                                           standard_price,
+                                           queen_count,
+                                           queen_price,
+                                           king_count,
+                                           king_price)
             
             # add and commit the new hotel to database
             try:
@@ -480,29 +367,18 @@ class SingleHotel(Resource):
                 hotel.Spa = has_spa
                 hotel.Bussiness_Office = has_bus
                 hotel.Wifi = has_wifi
-            if args["room_types"]:
-                room_types = request.json["room_types"]
-                for room in room_types:
-                    # check to see if the room types are valid
-                    for key, value in room.items():
-                        if key not in valid_room_types:
-                            abort(400, description = f"Room type ({key}) is not valid.")
-                # get the values
-                standard_count, standard_price, queen_count, queen_price, king_count, king_price = generate_hotel_room_type(room_types)
-                # update the hotel with the new values if they got changed
-                if standard_count != -1:
-                    hotel.standard_count = standard_count
-                if standard_price != -1:
-                    hotel.standard_price = standard_price
-                if queen_count != -1:
-                    hotel.queen_count = queen_count
-                if queen_price != -1:
-                    hotel.queen_price = queen_price
-                if king_count != -1:
-                    hotel.king_count = king_count
-                if king_price != -1:
-                    hotel.king_price = king_price
-            
+            if args["standard_count"]:
+                hotel.standard_count = request.json["standard_count"]
+            if args["standard_price"]:
+                hotel.standard_count = request.json["standard_price"]
+            if args["queen_count"]:
+                hotel.standard_count = request.json["queen_count"]
+            if args["queen_price"]:
+                hotel.standard_count = request.json["queen_price"]
+            if args["king_count"]:
+                hotel.standard_count = request.json["king_count"]
+            if args["king_price"]:
+                hotel.standard_count = request.json["king_price"]
             # commit the changes to the database
             try:
                 session.commit()
