7964595f17ceb86375d8867aa4750e31b87b7cde
iirlas <bitwisetuna@gmail.com>
2021-11-04 08:18:04 -0500

    Revert "Update index.js"
    This reverts commit 55d7731b67e4c73f7efb15e8a696d5e6e77581a1.

--- user-api/index.js
+++ user-api/index.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const db = require("./models");
 
+app.use(
+  cors({ origin: process.env.REACT_APP_URL, exposedHeaders: "x-auth-token" })
+);
 app.use(express.json());
 
 db.sequelize.sync();
