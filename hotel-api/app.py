006bd9181298c4da6560a39915fcd099470dc6ea
Alaroudi <alaroudi.abdel@gmail.com>
2021-11-08 12:49:51 -0600

    Change the hotel availability route endpoint to fix cors

--- hotel-api/app.py
+++ hotel-api/app.py
@@ -525,7 +525,7 @@ GROUP BY hotel_id)
 # add to each class to API
 api.add_resource(SingleHotel, "/api/hotels/<int:hotel_id>")
 api.add_resource(AllHotels, "/api/hotels")
-api.add_resource(HotelAvailability, "/api/hotel-availability/")
+api.add_resource(HotelAvailability, "/api/hotels/availability")
 
 if __name__ == "__main__":
     # load dotenv
